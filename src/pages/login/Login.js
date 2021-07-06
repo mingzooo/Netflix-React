10230b22772e0371b8c2a5f3b4808a7b697d45ac
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-06 18:13:08 +0900

    [fix] container rebuild, align center '도움이 필요하신가요'

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -3,8 +3,10 @@ import "./login.scss";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import FacebookIcon from '@material-ui/icons/Facebook';
+import CheckBoxIcon from '@material-ui/icons/CheckBox';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 
-const Login = () => {
+const Login1 = () => {
   const [check, setCheck] = useState({ checkBox: true });
 
   const handleChange = (event) => {
@@ -24,38 +26,39 @@ const Login = () => {
       </div>
       <div className="container">
         <form>
-          <h1>로그인</h1>
-          <input type="email" placeholder="이메일 주소 또는 전화번호" />
-          <input type="password" placeholder="비밀번호" />
-          <button>로그인</button>
-          <div>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={check.checkbox}
-                  onChange={handleChange}
-                  name="checkBox"
-                  color="primary"
+          <div className="login-container">
+            <h1>로그인</h1>
+            <input type="email" placeholder="이메일 주소 또는 전화번호" />
+            <input type="password" placeholder="비밀번호" />
+            <button>로그인</button>
+            <div className="remember">
+              <small>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      icon={<CheckBoxOutlineBlankIcon fontSize="small" />}
+                      checkedIcon={<CheckBoxIcon fontSize="small" />}
+                      name="checkedI"
+                      color="#13c552"
+                    />
+                  }
+                  label={<span style={{ fontSize: '13.5px' }}>로그인 정보 저장</span>}
                 />
-              }
-              label="로그인 정보 저장"
-            />
-            <small>도움이 필요하신가요?</small>
+              </small>
+              <small>도움이 필요하신가요?</small>
+            </div>
+            <div className="more">
+              <span><FacebookIcon color="primary" />  Facebook으로 로그인</span>
+              <span>
+                Netflix 회원이 아닌가요? <b>지금 가입하세요.</b>
+              </span>
+              <small>이 페이지는 Google reCAPTCHA의 보호를 받아 사용자가 로봇이 아님을 확인합니다. <span>자세히 알아보기.</span></small>
+            </div>
           </div>
-          <a>
-            <FacebookIcon color="primary" />
-            <span>Facebook으로 로그인</span>
-          </a>
-          <span>
-            Netflix 회원이 아닌가요? <b>지금 가입하세요.</b>
-          </span>
-          <small>
-            이 페이지는 Google reCAPTCHA의 보호를 받아 사용자가 로봇이 아님을 확인합니다. <span>자세히 알아보기</span>
-          </small>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login1;
