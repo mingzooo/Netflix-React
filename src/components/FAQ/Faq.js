3244612bd400aaea0527868d9e233ccb46319f91
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-07 15:10:18 +0900

    [feat] FAQ, input in register page

--- src/components/FAQ/Faq.js
+++ src/components/FAQ/Faq.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./faq.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { CSSTransition } from 'react-transition-group'
 
 import { AddIcon, RemoveIcon } from '@material-ui/icons/Add';
-import { CSSTransition } from 'react-transition-group';
 
-const faqComponents = (props) => {
+const FaqComponent = props => {
     const { faqOpenHandler, text, boxOpen, boxText } = props
     return (
         <>
@@ -14,7 +15,7 @@ const faqComponents = (props) => {
                 onClick={faqOpenHandler}
             >
                 <div>{text}</div>
-                <div icon={boxOpen ? RemoveIcon : AddIcon} />
+                <FontAwesomeIcon icon={boxOpen ? RemoveIcon : AddIcon} />
             </div>
 
             <CSSTransition in={boxOpen} classNames="faq-animation" timeout={500} unmountOnExit>
@@ -26,4 +27,4 @@ const faqComponents = (props) => {
     );
 };
 
-export default faqComponents;
+export default FaqComponent;
