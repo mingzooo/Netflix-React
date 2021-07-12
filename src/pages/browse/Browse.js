6395d8f84a950f17653d9acde3d4262c7c8183ff
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-12 23:30:02 +0900

    [feat] Ranking Component

--- src/pages/browse/Browse.js
+++ src/pages/browse/Browse.js
@@ -2,9 +2,9 @@
 import Navbar from "../../components/navbar/Navbar";
 import Featured from "../../components/featured/Featured";
 import "./browse.scss";
+import Rank from "../../components/rank/Rank";
 import List from "../../components/list/List";
 import Footer from "../../components/footer/Footer";
-import { useHistory } from 'react-router-dom';
 
 
 const Browse = () => {
@@ -14,10 +14,12 @@ const Browse = () => {
       <div className="home">
         <Navbar />
         <Featured />
+        <Rank category="오늘 한국의 TOP10 콘텐츠" />
         <List category="오늘 넷플릭스에서" />
         <List category="넷플릭스 인기 콘텐츠" />
         <List category="지금 뜨는 콘텐츠" />
         <List category="미국 TV프로그램" />
+        <List category="애니메이션" />
       </div>
       <Footer />
     </div>
