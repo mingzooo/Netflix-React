ed22857702c34eda04b16f08ae378c7c3978340b
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-18 15:19:52 +0900

    [feat] redux reducer

---
+++ src/store/reducers/slices/animations.js
@@ -0,0 +1,7 @@
+import { ANIMATION } from './types';
+
+const animation = () => {
+    return {
+        type: 'ANIMATION'
+    }
+}
---
+++ src/store/reducers/store.js
@@ -0,0 +1,24 @@
+import { createStore } from 'redux'
+
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import trendingReducer from './slices/trendingSlice'
+import topRatedReducer from './slices/topratedSlice.js'
+import netflixOriginalsReducer from './slices/netflixOriginalsSlice'
+import moviesByGenresReducer from './slices/moviesByGenreSlice'
+import tvByGenresReducer from './slices/tvByGenreSlice'
+import latestVideoReducer from './slices/latestVideoSlice'
+
+const store = configureStore({
+    reducer: {
+        trending: trendingReducer,
+        toprated: topRatedReducer,
+        netflixOriginals: netflixOriginalsReducer,
+        moviesByGenre: moviesByGenresReducer,
+        tvByGenre: tvByGenresReducer,
+        latestVideos: latestVideoReducer
+    },
+    // Clear this in production, as it is done by default 
+    middleware: [...getDefaultMiddleware({ immutableCheck: false })]
+})
+
+export default store;
