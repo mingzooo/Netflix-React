53dec21e20ddc1a260bcb68eba76db7e14562b52
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-20 23:17:09 +0900

    [remove] browse

--- src/pages/browse/routes/List.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const List = () => {
-    return (
-        <div style={{ color: 'white', paddingTop: '95px', textAlign: 'center' }}>Coming soon</div>
-    )
-}
-
-export default List
