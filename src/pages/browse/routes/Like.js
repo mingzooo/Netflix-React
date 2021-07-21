de9ba4dc2b0e5657b96c2e1d8b106843d56e6b6a
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-07-22 01:00:48 +0900

    [feat] like page

---
+++ src/pages/browse/routes/Like.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../../components/navbar/Navbar';
+
+const Like = () => {
+    return (
+        <div className="main">
+            <Navbar />
+            <div style={{ color: 'white', paddingTop: '95px', textAlign: 'center' }}>Coming soon</div>
+        </div>
+    )
+}
+
+export default Like;
